dec1838b5bb4e07df5d2461600d14153a3ddf2f4
jadsonsts <jadson.s.carneiro@gmail.com>
2022-07-03 22:26:05 +1200

    first commit

---
+++ FoodManChu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodManChu
+//
+//  Created by Jadson on 3/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
